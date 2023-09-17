dd7b0c951d5a7021b08e8de2babd2cfb4aad6ab8
Reyniel Mahinay <reyniel.t.mahinay@gmail.com>
2023-09-17 12:05:17 +0800

    resume download

--- src/components/Reveal.jsx
+++ src/components/Reveal.jsx
@@ -11,7 +11,13 @@ const Reveal = ({ children, width = "fit-content" }) => {
       ([entry]) => {
         if (entry.isIntersecting) {
           setIsVisible(true);
-          controls.start({ opacity: 1, y: 0 }); // Start the animation
+          controls.start({
+            opacity: 1,
+            y: 0,
+            transition: {
+              delay: 0.2,
+            },
+          }); // Start the animation
         }
       },
       { threshold: 0.5 } // Adjust the threshold as needed
@@ -31,11 +37,7 @@ const Reveal = ({ children, width = "fit-content" }) => {
 
   return (
     <div ref={ref}>
-      <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={controls}
-        style={{ opacity: isVisible ? 1 : 0, y: isVisible ? 0 : 20 }}
-      >
+      <motion.div initial={{ opacity: 0, y: 20 }} animate={controls}>
         {children}
       </motion.div>
     </div>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BackgroundParticles } from "../components";
 import Reveal from "../components/Reveal";
+import resume from "../assets/Mahinay, Reyniel T.pdf";
 
 const Hero = () => {
   return (
@@ -10,21 +11,29 @@ const Hero = () => {
       id="home"
     >
       <BackgroundParticles />
-      <div className=" w-full pr-8 max-sm:pr-0 relative ">
+      <div className=" w-full pr-8 max-sm:pr-0 relative flex flex-col items-end">
         <h1
           className="font-montserrat tracking-tight	 text-9xl max-sm:text-7xl 
-        font-black flex flex-1 justify-end max-sm:justify-start max-sm:pl-5 max-sm:pt-[90px] text-black-gray"
+        font-black flex flex-1  max-sm:justify-start max-sm:pl-5 max-sm:pt-[90px] text-black-gray"
         >
           HELLO
         </h1>
+
+        <a href={resume} download="Mahinay, Reyniel T.pdf">
+          <div className=" bg-black-gray group text-semi-white  mt-[165px] hover:text-black-gray hover:bg-transparent">
+            <p className="element font-montserrat  group-hover:translate-x-5 transition-transform  tracking-tight text-4xl font-black px-5 ">
+              RESUME
+            </p>
+          </div>
+        </a>
       </div>
-      <div className="w-full pl-3 pt-[203px] max-sm:pt-[10px] max-sm:pl-6  relative">
+      <div className="w-full pl-3 pt-[115px] max-sm:pt-[10px] max-sm:pl-6  relative">
         <h3 className="font-montserrat tracking-tight	 font-extrabold text-6xl max-sm:text-3xl text-black-gray">
           I'M REYNIEL <br /> MAHINAY
         </h3>
         <p
           className="font-extrabold tracking-tight	 text-4xl max-sm:text-xl 
-        pt-[96px] max-sm:pt-5 text-black-gray "
+        pt-[185px] max-sm:pt-5 text-black-gray "
         >
           FRONT-END DEVELOPER
         </p>
